21ac117bb5966fd05c15de7a1746b5155c7e1ee6
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-16 23:27:53 +0900

    feat: add delete button component

---
+++ src/app/favorites/deleteButton.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+interface Props {
+  ideaId: number;
+}
+
+const deleteIdea = async (router: any, ideaId: number) => {
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/idea`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ideaId }),
+    });
+    window.alert('削除しました');
+    router.refresh();
+  }
+};
+
+export default function DeleteButton({ ideaId }: Props) {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => deleteIdea(router, ideaId)}
+      className="bg-green-300 rounded-md py-1 px-4"
+    >
+      削除
+    </button>
+  );
+}
