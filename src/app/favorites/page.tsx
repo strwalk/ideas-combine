87097fe379771042c09366370b811b9da55549d9
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-19 00:36:01 +0900

    feat: add top page link to favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import prisma from '../_lib/connectToPrisma';
 import DeleteButton from './deleteButton';
 
@@ -53,6 +54,28 @@ export default async function Favorites() {
               ) : (
                 <p>保存したアイデアはありません</p>
               )}
+            </section>
+            <section className="flex justify-center mt-6">
+              <Link
+                href="/"
+                className="border border-gray-400 hover:bg-gray-50 py-2 px-6 flex items-center gap-1 text-sm sm:text-base lg:text-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 19.5L8.25 12l7.5-7.5"
+                  />
+                </svg>
+                <span>Top Page</span>
+              </Link>
             </section>
           </section>
         </section>
