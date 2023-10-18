a610ae67c095f9524075063b4a36ccc5a15f5143
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-19 00:17:53 +0900

    fix: save button design

--- src/app/_components/saveButton.tsx
+++ src/app/_components/saveButton.tsx
@@ -27,9 +27,22 @@ export default function SaveButton({ word1Id, word2Id }: Props) {
   return (
     <button
       onClick={() => saveIdea(word1Id, word2Id)}
-      className="text-xl bg-red-300 hover:bg-red-400 rounded-md shadow py-3 px-4"
+      className="bg-yellow-200 hover:bg-yellow-100 rounded-md shadow py-2 md:py-3 px-2 md:px-4"
     >
-      保存
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+        />
+      </svg>
     </button>
   );
 }
