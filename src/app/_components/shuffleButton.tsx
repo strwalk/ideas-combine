8089dd1bb3b01432980c17c36df198da8aea6535
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-22 00:49:41 +0900

    fix: shuffle button design

--- src/app/_components/shuffleButton.tsx
+++ src/app/_components/shuffleButton.tsx
@@ -8,15 +8,16 @@ export default function ShuffleButton() {
   return (
     <button
       onClick={() => router.refresh()}
-      className="bg-sky-200 hover:bg-sky-100 py-2 sm:py-3 px-12 rounded shadow"
+      className="border border-gray-400 hover:bg-gray-50 py-2 px-8 flex items-center gap-2.5 text-gray-500"
     >
+      <span className="text-sm sm:text-base lg:text-lg">Shuffle</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="w-5 sm:w-6 h-5 sm:h-6"
+        className="w-5 sm:w-6 h-5 sm:h-6 font-bold"
       >
         <path
           strokeLinecap="round"
