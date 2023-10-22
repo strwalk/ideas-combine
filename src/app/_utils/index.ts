e7ce1aefdd01cf1d7a19e7f65e7f8e1e2cb6f204
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-22 13:37:36 +0900

    feat: add findWord function to utils

--- src/app/_utils/index.ts
+++ src/app/_utils/index.ts
@@ -1,8 +1,10 @@
+import wordsJson from '../_lib/words.json';
+
 const generateRandomNumber = (maxSize: number) => {
   return Math.floor(Math.random() * maxSize + 1);
 };
 
-const convertToTwoDimensionalArray = (array: number[], size: number) => {
+const convertToTwoDimensionalArray = (array: Word[], size: number) => {
   const result = [];
   for (let i = 0; i < array.length; i += size) {
     result.push(array.slice(i, i + size));
@@ -10,4 +12,7 @@ const convertToTwoDimensionalArray = (array: number[], size: number) => {
   return result;
 };
 
-export { generateRandomNumber, convertToTwoDimensionalArray };
+const findWord = (wordId: number) =>
+  (wordsJson.find((word) => word.id === wordId) ?? wordsJson[0]).word;
+
+export { generateRandomNumber, convertToTwoDimensionalArray, findWord };
