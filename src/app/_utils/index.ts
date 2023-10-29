80187824e774d7a6d022142c5fa8f1654d8f7137
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-29 18:18:11 +0900

    fix: remove unnecessary function

--- src/app/_utils/index.ts
+++ src/app/_utils/index.ts
@@ -4,15 +4,7 @@ const generateRandomNumber = (maxSize: number) => {
   return Math.floor(Math.random() * maxSize + 1);
 };
 
-const convertToTwoDimensionalArray = (array: Word[], size: number) => {
-  const result = [];
-  for (let i = 0; i < array.length; i += size) {
-    result.push(array.slice(i, i + size));
-  }
-  return result;
-};
-
 const findWord = (wordId: number) =>
   (wordsJson.find((word) => word.id === wordId) ?? wordsJson[0]).word;
 
-export { generateRandomNumber, convertToTwoDimensionalArray, findWord };
+export { generateRandomNumber, findWord };
