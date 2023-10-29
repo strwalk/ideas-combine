43852e1679c226762eba20e7f70e9163e0e1a9d2
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-29 17:54:40 +0900

    fix: word cards design

--- src/app/_components/wordCards.tsx
+++ src/app/_components/wordCards.tsx
@@ -1,57 +1,42 @@
-import { getUserId } from '../_lib/auth';
-import SaveButton from './saveButton';
-
 interface WordCardProps {
-  word: Word;
-  userId: string;
+  word: string;
 }
 
 interface WordCardsProps {
-  words: Word[];
+  wordsList: Word[];
 }
 
-function WordCard({ word, userId }: WordCardProps) {
-  const defaultCardWidth = userId ? 'w-[6rem]' : 'w-[7rem]';
-
+function WordCard({ word }: WordCardProps) {
   return (
-    <div
-      className={`${defaultCardWidth} sm:w-[9rem] md:w-[10rem] lg:w-[12rem] border border-gray-300 py-2 md:py-3 lg:py-4 px-2 sm:px-3 h-full flex justify-center items-center`}
-    >
-      <p>{word.word}</p>
+    <div className="border border-gray-300 px-3 py-3 sm:py-3.5 flex justify-center items-center">
+      <p>{word}</p>
     </div>
   );
 }
 
-export default async function WordCards({ words }: WordCardsProps) {
-  const userId = await getUserId();
-
+export default async function WordCards({ wordsList }: WordCardsProps) {
   return (
-    <>
-      <WordCard word={words[0]} userId={userId} />
-      <p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-4 md:w-5 lg:w-6 h-4 md:h-5 lg:h-6 text-gray-600 sm:text-gray-500"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </p>
-      <WordCard word={words[1]} userId={userId} />
-      {userId && (
-        <SaveButton
-          userId={userId}
-          word1Id={words[0].id}
-          word2Id={words[1].id}
-        />
-      )}
-    </>
+    <section>
+      <section className="flex flex-col gap-y-4 text-lg">
+        <WordCard word={wordsList[0].word} />
+        <div className="flex justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-gray-600 sm:text-gray-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+        <WordCard word={wordsList[1].word} />
+      </section>
+    </section>
   );
 }
